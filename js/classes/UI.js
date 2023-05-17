8764631ba78cbf479e9d12321d2817881537ce24
Fabian <contacto.fabianmartinezcaro@gmail.com>
2023-05-17 14:19:28 -0300

    agregando estilos al card

--- js/classes/UI.js
+++ js/classes/UI.js
@@ -21,20 +21,23 @@ export default class UI{
 
             resultado.innerHTML += `
                 <div class="w-full md:w-1/2 lg:w-1/4">
-                    <div class="h-auto rounded-lg m-4 p-2 shadow-lg bg-white">
-                        <img class="w-full rounded-md" src="${previewURL}">
-                        <div class="flex flex-row space-x-2 text-sm mt-2">
-                            <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${likes}</span> Me Gustas</p>
-                            <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${numsToK(downloads)}</span> Descargas</p>
+                    <div class="h-auto rounded-lg m-4 shadow-lg bg-white">
+                        <div class="space-x-2 text-sm mt-2">
+                            <img class="w-full rounded-t-lg" src="${previewURL}">
+                            <p class="text-gray-600 font-light text-xs mt-2"><span class="font-bold">Visto:</span> ${views} veces</p>
+                            <div class="px-2 mt-2 text-xs border-l-4 border-blue-400">
+                                <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${likes}</span> Me Gustas</p>
+                                <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${numsToK(downloads)}</span> Descargas</p>
+                            </div>
                         </div>
 
                         <div class="">
                             
                         </div>
 
-                        <div>
-                            <button class="rounded-md bg-blue-800 text-white text-sm mt-4 py-1 px-4" href=${previewURL}>Descargar</button>
-                            <button class="rounded-md bg-gray-800 text-white text-sm mt-4 py-1 px-4">Ver Imagen</button>
+                        <div class="flex justify-center space-x-2 pb-4">
+                            <button class="focus:outline-none hover:bg-blue-800 duration-100 rounded-md bg-blue-600 text-white text-sm mt-4 py-1 px-4" href=${previewURL}>Descargar</button>
+                            <button class="focus:outline-none hover:bg-blue-800 duration-100 hover:duration-100 rounded-md bg-gray-600 text-white text-sm mt-4 py-1 px-4">Ver Imagen</button>
                         </div>
                     </div>
                 </div>
