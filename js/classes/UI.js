82de1501da4e631428a5bd8cbe7edc9018a05834
Fabian <contacto.fabianmartinezcaro@gmail.com>
2023-05-17 11:56:19 -0300

    agregando botones al card

--- js/classes/UI.js
+++ js/classes/UI.js
@@ -11,13 +11,30 @@ export default class UI{
             console.log(imagen)
             const {comments, downloads, likes, views, largeImageURL, previewURL} = imagen;
 
+            function numsToK(numero){
+                if(numero > 10000){
+                    let resultado = numero / 10000;
+                    resultado = resultado.toFixed(1) + 'k';
+                    return resultado;
+                }
+            }
+
             resultado.innerHTML += `
-                <div class="w-1/2 md:w-1/3 lg:w-1/4">
-                    <div class="m-4 p-2 border-2 border-gray-800">
-                        <img class="w-full border-1 border-gray-800" src="${previewURL}">
-                        <div class="flex flex-row space-x-2 text-sm">
+                <div class="w-full md:w-1/2 lg:w-1/4">
+                    <div class="h-auto rounded-lg m-4 p-2 shadow-lg bg-white">
+                        <img class="w-full rounded-md" src="${previewURL}">
+                        <div class="flex flex-row space-x-2 text-sm mt-2">
                             <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${likes}</span> Me Gustas</p>
-                            <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${comments}</span> Comments</p>
+                            <p class="font-medium text-gray-600"><span class="font-bold text-blue-600">${numsToK(downloads)}</span> Descargas</p>
+                        </div>
+
+                        <div class="">
+                            
+                        </div>
+
+                        <div>
+                            <button class="rounded-md bg-blue-800 text-white text-sm mt-4 py-1 px-4" href=${previewURL}>Descargar</button>
+                            <button class="rounded-md bg-gray-800 text-white text-sm mt-4 py-1 px-4">Ver Imagen</button>
                         </div>
                     </div>
                 </div>
